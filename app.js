3585505dfa444843a680ee1fb424416806e5af89
Kel <me@ienablemuch.com>
2014-11-02 20:38:13 +0800

    Angular Ajax Combobox
    On-demand loading and paged list

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = angular.module('TheApp', ['ngResource', 'kel.ui']);
+
+
+
+app.controller('TheController', ['$http', '$resource', '$scope', function($http, $resource, $scope) {
+    var vm = this;
+
+    vm.message = "Hello";
+
+
+    vm.personId = 5500;
+    vm.personName = 'Aaron Butler';
+
+    vm.sum = 1 + 4;
+
+    vm.resultList = [];
+    vm.resultTotalRows = 0;
+
+    var dataRest = $resource('http://localhost:63692/api/AdventureWorksPeople');
+
+    vm.resultGetter = function(e) {
+            // return  the promise
+            return dataRest.get({ userInput : e.userInput, pageNumber : e.pageNumber }, function(result) {
+                vm.resultList = result.persons;
+                vm.resultTotalRows = result.totalRows;
+            });
+    };
+
+}]);
